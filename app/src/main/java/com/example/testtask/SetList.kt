ed6ef60455f691fed8701a1df0d4d9d55c4d2b4b
Harpreet456-Kaur <125293917+Harpreet456-Kaur@users.noreply.github.com>
2023-02-25 15:37:27 +0530

    changes in list

--- app/src/main/java/com/example/testtask/SetList.kt
+++ app/src/main/java/com/example/testtask/SetList.kt
@@ -1,21 +1,24 @@
 package com.example.testtask
 
 import android.app.Dialog
+import android.content.ClipData.Item
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Layout
+import android.view.LayoutInflater
 import android.widget.ArrayAdapter
 import androidx.core.view.get
 import com.example.testtask.databinding.ActivityMainBinding
 import com.example.testtask.databinding.ActivitySetlistBinding
+import java.io.File
 
 class SetList : AppCompatActivity() {
 
     lateinit var binding: ActivitySetlistBinding
     lateinit var sharedPreferences: SharedPreferences
-    //lateinit var userlist: ArrayList<String>
-    //lateinit var adapter:ArrayAdapter<String>
+    lateinit var adapter:ArrayAdapter<String>
+    var array = ArrayList<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,14 +26,21 @@ class SetList : AppCompatActivity() {
         val sharedPreferences=getSharedPreferences("File", MODE_PRIVATE)
 
         setContentView(binding.root)
-        val name=sharedPreferences.getInt("File",0)
+
+        val name=sharedPreferences.getInt("File", Int.MIN_VALUE)
+       // var array= arrayOf("1","2","3","4","5")
+        //for loop
+        adapter=ArrayAdapter(this,android.R.layout.simple_list_item_1,array)
+        binding.lvList.adapter=adapter
 
 
-        //adapter=ArrayAdapter(this,android.R.layout.simple_list_item_1,userlist)
-        //binding.lvList.adapter=adapter
-        binding.btnAdd.setOnClickListener {
-            var customDialog=Dialog(this)
-            var dialogBinding=layoutInflater
+        binding.lvList.setOnItemClickListener { adapterView, view, i, l ->
+            val dialogBinding=layoutInflater.inflate(R.layout.customdialog,null)
+            val mydialog=Dialog(this)
+            mydialog.setContentView(dialogBinding)
+            mydialog.setCancelable(true)
+            mydialog.show()
+        }
+
         }
         }
-    }
